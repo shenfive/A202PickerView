1b54e2d4322a8d283dae58db3d4c494c1b245ef6
Danny Shen <shenfive@gmail.com>
2022-01-23 15:53:53 +0800

    selection

--- A202PickerView/ViewController.swift
+++ A202PickerView/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-
+    var astrological = ["請選擇你的星座","白羊宮","金牛宮","雙子宮","巨蟹宮","獅子宮","處女宮","天秤宮","天蠍宮","射手宮","摩羯宮","水瓶宮","雙魚宮"]
+       var bloudType = ["請選擇你的血型","A","B","O","AB"]
 
     @IBOutlet weak var myPickerView: UIPickerView!
     
@@ -22,16 +23,35 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     //MARK: Picker View Data Source & Delegate
     
+    //有幾欄？
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5
+        switch component{
+        case 0:
+            return bloudType.count
+        case 1:
+            return astrological.count
+        default:
+            return 0
+        }
+        
+        
+        
+        
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "Hello!"
+        switch component{
+        case 0:
+            return bloudType[row]
+        case 1:
+            return astrological[row]
+        default:
+            return ""
+        }
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print("row:\(row)  component:\(component)")
