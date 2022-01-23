a3cbe372e24b7f98f0a993fed2db92c8b4f8f589
Danny Shen <shenfive@gmail.com>
2022-01-23 16:26:18 +0800

    Page 2 Class

---
+++ A202PickerView/Page2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Page2ViewController.swift
+//  A202PickerView
+//
+//  Created by 申潤五 on 2022/1/23.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("page2")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
